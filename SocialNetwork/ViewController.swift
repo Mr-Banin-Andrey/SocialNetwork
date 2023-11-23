ba601591006346d1e0dfbf048d7f8cd7f898d425
Andrey Banin <109150204+Mr-Banin-Andrey@users.noreply.github.com>
2023-11-23 17:47:13 +0300

    Initial Commit

---
+++ SocialNetwork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialNetwork
+//
+//  Created by Андрей Банин on 23.11.23..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
